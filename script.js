f2ac3bc87b27a5e14f3bab92632cd97858d1bf56
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-09-15 23:08:41 -0500

    current day js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-    
+
+}
+// current day
+
+const currentDate = $('#currentDay');
+currentDate.text(now);
+
     $('.save').click(function(){
         console.log('click')
     })
-})
 
 localStorage.setItem(key: hour)
 
