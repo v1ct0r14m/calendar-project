b622e34e70608aca28771d2482ca2f1bbe47de63
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-09-15 23:29:35 -0500

    hour class and button noticed/updated

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
-
-}
 // current day
 
 const currentDate = $('#currentDay');
-currentDate.text(now);
+currentDate.text
 
-    $('.save').click(function(){
+
+
+   
+// local storage lord
+{
+$('.save').click(function(){
         console.log('click')
     })
 
+
+
+
 localStorage.setItem(key: hour)
 
+
+}
 //parent siblings javascript 4 lyfe
