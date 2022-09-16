9d8a1ac3282f7c04d83b837a5c0de73abe309c43
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-09-15 23:54:53 -0500

    small additions, parent/sibling attempt

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = NowMoment;
 let currentHour = moment().format("HH");
 
+});
 
-   
+
+
+// parent siblings javascript 4 lyfe
+$('.save').click(function (event) {
+    event.displayDate();
+    var value = $(this).siblings(".hour").val();
+    var time = $(this).parent().attr("id").split("-")[1]
+    localStorage.setItem(time, value);
+
+});
+
 // local storage lord
-$('.save').click(function(){
-        console.log('click')
-    })
+$("#900 .time-block").val(localStorage.getItem("0900"));
 
 
 
-
-localStorage.setItem(key:hour, value:day)
-
-
-//parent siblings javascript 4 lyfe
+});
