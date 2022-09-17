f8254ff823302dc64d25a27bb723838ff7e10540
v1ct0r14m <tori.a.g.mota@gmail.com>
2022-09-16 23:17:17 -0500

    local storage added, understanding not

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-//MOMENT FOR DATE AND TIME
+//MOMENT FOR DATE AND TIME****
 let NowMoment = moment().format("MMMM do YYYY");
 let displayDate = document.getElementById("currentDay");
 displayDate.innerHTML = NowMoment;
-let currentHour = moment().format("HH");
+let currentDay = moment().format("HH");
 
 });
 
@@ -20,7 +20,14 @@ $('.save').click(function (event) {
 
 // local storage lord
 $("#900 .time-block").val(localStorage.getItem("0900"));
-
-
+$("#1000 .time-block").val(localStorage.getItem("1000"));
+$("#1100 .time-block").val(localStorage.getItem("1100"));
+$("#1200 .time-block").val(localStorage.getItem("1200"));
+$("#1300 .time-block").val(localStorage.getItem("1300"));
+$("#1400 .time-block").val(localStorage.getItem("1400"));
+$("#1500 .time-block").val(localStorage.getItem("1500"));
+$("#1600 .time-block").val(localStorage.getItem("1600"));
+$("#1700 .time-block").val(localStorage.getItem("1700"));
+$("#1800 .time-block").val(localStorage.getItem("1800"));
 
 });
